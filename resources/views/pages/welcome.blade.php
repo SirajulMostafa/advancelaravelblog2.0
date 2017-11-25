8d95a028838ad6d80abdde75d37de43784f4f41b
SirajulMostafa <sirajulmost@gmail.com>
2017-11-25 23:49:22 +0600

    -- change view static to dynamic--

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,77 +1,42 @@
-@extends('layouts.main')
-@section('title',' | Home')
+@extends('layout.main')
+@section('title',' |welcome')
 @section('content')
 
   <div class="row">
     <div class="col-md-12">
       <div class="jumbotron">
-  <h1 class="display-3">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <h1 class="display-3">Wellcome  to my Laravel blog Site</h1>
+  <p class="lead">
+    This is a Blog site, this is my Laravel blog site make with laravel5.5.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
   <p class="lead">
-    <a class="btn btn-info btn-lg" href="#" role="button">popular post</a>
+    <a class="btn btn-info  btn-lg" href="#" role="button">popular post</a>
   </p>
 </div>
     </div>
   </div>
   <div class="row">
     <div class="col-md-8">
+      @foreach ($posts as $post)
       <div class="post">
-        <h3>Post Title</h3>
-        <p class="">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <h3>{{$post->title}}</h3>
+          <span class="fa fa-user-o">{{$post->user_id}}</span>
+        <p class="details">
+          {{-- {{$post->body}} --}}
+          {{substr(strip_tags($post->body),0,300)}}{{strlen(strip_tags($post->body))>300?".....":""}}
         </p>
-        <a class=" btn btn-info" href="#" role="button">Read more</a>
+        <a class=" btn btn-info" href="{{url('blogs/'.$post->slug)}}" role="button">Read more</a>
 
       </div>
-      <hr>
-      <div class="post">
-        <h3>Post Title</h3>
-        <p class="">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <a class=" btn btn-info" href="#" role="button">Read more</a>
-
-      </div>
-      <hr>
-      <div class="post">
-        <h3>Post Title</h3>
-        <p class="">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <a class=" btn btn-info" href="#" role="button">Read more</a>
-
-      </div>
-      <hr>
-      <div class="post">
-        <h3>Post Title</h3>
-        <p class="">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <a class=" btn btn-info" href="#" role="button">Read more</a>
-
-      </div>
+        @endforeach
       <hr>
     </div>
     <div class="col-md-3 col-md-offset-1">
       <h3>Sidebar</h3>
   Lorem ipsum dolor sit amet, consectetur adipisicing elit,
   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
     </div>
   </div>
     </div>
